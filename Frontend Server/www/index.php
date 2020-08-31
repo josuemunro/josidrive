879c635be1a4eedfab9a35455c9fb8f312644218
josuemunro <josuemunro@gmail.com>
2020-08-31 22:15:03 +1200

    Added provisioning to frontendApache VM, added frontend conf file to enable our index.php site and added the start of our php site

---
+++ Frontend Server/www/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Ligma</title>
+</head>
+<body>
+<h1>Yeah the boys</h1>
+
+<form enctype="multipart/form-data" action="uploader.php" method="POST">
+Choose a file to upload: <input name="file" type="file" /><br />
+<input type="submit" value="Upload File" />
+</form>
+
+</body>
+</html>
