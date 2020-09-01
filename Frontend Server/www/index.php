1e9d782ebca42d6b0978ac7307a84e61a5905f5e
josuemunro <josuemunro@gmail.com>
2020-09-01 12:36:09 +1200

    Added PHP testing code, had a lot of trouble connecting to sql, works now

--- Frontend Server/www/index.php
+++ Frontend Server/www/index.php
@@ -11,6 +11,24 @@
 Choose a file to upload: <input name="file" type="file" /><br />
 <input type="submit" value="Upload File" />
 </form>
+<?php
+$servername = "192.168.3.11";
+$username = "webuser";
+$password = "insecure_db_pw";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=josidrive", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  $q = $conn->query("SELECT * FROM files");
+
+  while($row = $q->fetch()){
+  echo "<p>".$row["path"]."</p><p>".$row["name"]."</p>";
+}
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
 </body>
 </html>
