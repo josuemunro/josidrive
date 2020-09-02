b4d94444f5df389441fbbe291d2231ac95ade508
josuemunro <josuemunro@gmail.com>
2020-09-02 14:15:58 +1200

    Cleaning up php files, moved code back into upload.php, deleted view.php and set a redirect on upload.php

--- Frontend Server/www/index.php
+++ Frontend Server/www/index.php
@@ -7,36 +7,18 @@
 <body>
 <h1>Yeah the boys</h1>
 
-<form method="post" enctype="multipart/form-data">
+<form action="upload.php" method="post" enctype="multipart/form-data">
     Select File to Upload:
     <input type="file" name="myfile">
     <button name="btn">Upload ya stuff</button>
 </form>
 <?php
   include 'dbConfig.php';
-  if(isset($_POST["btn"])){
-    echo "something happened ";
-    $name = $_FILES['myfile']['name'];
-    $type = $_FILES['myfile']['type'];
-    $data = addslashes(file_get_contents($_FILES['myfile']['tmp_name']));
-    try {
-      $sql = "INSERT INTO files (name, datatype, data)
-    VALUES ('$name', '$type', '{$data}')";
-      // use exec() because no results are returned
-      $conn->exec($sql);
-      echo "New record created successfully";
-    } catch(PDOException $e) {
-      echo $sql . "<br>" . $e->getMessage();
-    }
-  }
     
   $q = $conn->query("SELECT * FROM files");
 
   while($row = $q->fetch()){
   echo "<li>
-    <a target='_blank' href='view.php?id=".$row['id']."'>
-      ".$row["name"]."
-    </a>
     <embed src='data:".$row['datatype'].";base64,".base64_encode($row['data'])."' width='200'/>
   </li>
   <a target='_blank' href='download.php?id=".$row['id']."'>
--- Frontend Server/www/upload.php
+++ Frontend Server/www/upload.php
@@ -1,33 +1,19 @@
+<meta http-equiv="refresh" content="2; url=index.php">
 <?php
 include 'dbConfig.php';
-$statusMsg = '';
-
 if(isset($_POST["btn"])){
     echo "something happened ";
     $name = $_FILES['myfile']['name'];
     $type = $_FILES['myfile']['type'];
-    $data = file_get_contents($_FILES['myfile']['tmp_name']);
-    // $stmt = $conn->prepare("INSERT INTO files VALUES('', ?, ?, ?)");
-    // echo "we made some sort of connection ";
-    // $stmt->bindParam('name', $name);
-    // $stmt->bindParam('datatype', $type);
-    // $stmt->bindParam('data', $data);
-    // $stmt->execute();
-    // echo "what is going on ";
-    $sql = "INSERT INTO files VALUES ('John')";
-    echo "what in the world";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "huuuuh";
-    if($sql){
-        $statusMsg = "The file ".$name. " has been uploaded successfully.";
-    }else{
-        $statusMsg = "File upload failed, please try again.";
+    $data = addslashes(file_get_contents($_FILES['myfile']['tmp_name']));
+    try {
+      $sql = "INSERT INTO files (name, datatype, data)
+    VALUES ('$name', '$type', '{$data}')";
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      echo "New record created successfully";
+    } catch(PDOException $e) {
+      echo $sql . "<br>" . $e->getMessage();
     }
-}else{
-    $statusMsg = 'Please select a file to upload.';
-}
-
-// Display status message
-echo $statusMsg;
+  }
 ?>
