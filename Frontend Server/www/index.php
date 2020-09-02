79479630e00b0cc84f49f5e319b879720113cd5f
josuemunro <josuemunro@gmail.com>
2020-09-02 21:43:16 +1200

    Styling work and removed unnecessary php feedback

--- Frontend Server/www/index.php
+++ Frontend Server/www/index.php
@@ -3,31 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <title>Ligma</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-<h1>Yeah the boys</h1>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<div class="header">
+  <a href="index.php">
+    <img class="logo" src="logo.png">
+  </a>
+  <a href="upload.html">Upload</a>
+</div>
+<form action="uploader.php" method="post" enctype="multipart/form-data">
     Select File to Upload:
     <input type="file" name="myfile">
     <button name="btn">Upload ya stuff</button>
 </form>
+<div class="parent">
 <?php
   include 'dbConfig.php';
     
   $q = $conn->query("SELECT * FROM files");
 
   while($row = $q->fetch()){
-  echo "<li>
-    <embed src='data:".$row['datatype'].";base64,".base64_encode($row['data'])."' width='200'/>
-  </li>
-  <a target='_blank' href='download.php?id=".$row['id']."'>
-      Download here!
-    </a>";
+  echo "<div class='box'>
+    <embed src='data:".$row['datatype'].";base64,".base64_encode($row['data'])."'/>
+    <a class='download' target='_blank' href='download.php?id=".$row['id']."'>
+      Download ".$row['name']."
+    </a>
+  </div>";
 }
 
 ?>
-<br>
+</div>
 
 </body>
 </html>
--- Frontend Server/www/uploader.php
+++ Frontend Server/www/uploader.php
@@ -1,8 +1,8 @@
-<meta http-equiv="refresh" content="2; url=index.php">
+<meta http-equiv="refresh" content="1; url=index.php">
+<h1>Redirecting...</h1>
 <?php
 include 'dbConfig.php';
 if(isset($_POST["btn"])){
-    echo "something happened ";
     $name = $_FILES['myfile']['name'];
     $type = $_FILES['myfile']['type'];
     $data = addslashes(file_get_contents($_FILES['myfile']['tmp_name']));
@@ -11,7 +11,6 @@ if(isset($_POST["btn"])){
     VALUES ('$name', '$type', '{$data}')";
       // use exec() because no results are returned
       $conn->exec($sql);
-      echo "New record created successfully";
     } catch(PDOException $e) {
       echo $sql . "<br>" . $e->getMessage();
     }
